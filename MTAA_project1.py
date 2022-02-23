6a425d7fe722ed83137e9b16d5a5e3706e8c8499
Natalia Sedova <sedova.natalia9@gmail.com>
2022-02-23 21:00:40 +0100

    wrapping...

--- MTAA_project1.py
+++ MTAA_project1.py
@@ -1,15 +1,16 @@
-from .sipfullproxy import *
+import sipfullproxy
 
 if __name__ == "__main__":    
-    logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s',filename='proxy.log',level=logging.INFO,datefmt='%H:%M:%S')
-    logging.info(time.strftime("%a, %d %b %Y %H:%M:%S ", time.localtime()))
-    hostname = socket.gethostname()
-    logging.info(hostname)
-    ipaddress = socket.gethostbyname(hostname)
+    #logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s',filename='proxy.log',level=logging.INFO,datefmt='%H:%M:%S')
+    #logging.info(time.strftime("%a, %d %b %Y %H:%M:%S ", time.localtime()))
+    hostname = sipfullproxy.socket.gethostname()
+    #logging.info(hostname)
+    ipaddress = sipfullproxy.socket.gethostbyname(hostname)
     if ipaddress == "127.0.0.1":
-        ipaddress = sys.argv[1]
-    logging.info(ipaddress)
-    recordroute = "Record-Route: <sip:%s:%d;lr>" % (ipaddress,PORT)
-    topvia = "Via: SIP/2.0/UDP %s:%d" % (ipaddress,PORT)
-    server = socketserver.UDPServer((HOST, PORT), UDPHandler)
+        ipaddress = sipfullproxy.sys.argv[1]
+    sipfullproxy.logging.info(ipaddress)
+    sipfullproxy.recordroute = "Record-Route: <sip:%s:%d;lr>" % (ipaddress,sipfullproxy.PORT)
+    sipfullproxy.topvia = "Via: SIP/2.0/UDP %s:%d" % (ipaddress,sipfullproxy.PORT)
+    server = sipfullproxy.socketserver.UDPServer((sipfullproxy.HOST, sipfullproxy.PORT), sipfullproxy.UDPHandler)
+    print("Proxy is active")
     server.serve_forever()
