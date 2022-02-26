1e51643427a0c7058d946388ed6d6ece4ae5abc5
Natalia Sedova <sedova.natalia9@gmail.com>
2022-02-26 19:39:41 +0100

    IP proxy better

--- MTAA_project1.py
+++ MTAA_project1.py
@@ -1,16 +1,19 @@
 import sipfullproxy
 
 if __name__ == "__main__":    
-    #logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s',filename='proxy.log',level=logging.INFO,datefmt='%H:%M:%S')
-    #logging.info(time.strftime("%a, %d %b %Y %H:%M:%S ", time.localtime()))
-    hostname = sipfullproxy.socket.gethostname()
-    #logging.info(hostname)
-    ipaddress = sipfullproxy.socket.gethostbyname(hostname)
-    if ipaddress == "127.0.0.1":
-        ipaddress = sipfullproxy.sys.argv[1]
-    sipfullproxy.logging.info(ipaddress)
-    sipfullproxy.recordroute = "Record-Route: <sip:%s:%d;lr>" % (ipaddress,sipfullproxy.PORT)
-    sipfullproxy.topvia = "Via: SIP/2.0/UDP %s:%d" % (ipaddress,sipfullproxy.PORT)
-    server = sipfullproxy.socketserver.UDPServer((sipfullproxy.HOST, sipfullproxy.PORT), sipfullproxy.UDPHandler)
+    sipfullproxy.logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s',filename='proxy.log',level=sipfullproxy.logging.INFO,datefmt='%H:%M:%S')
+    sipfullproxy.logging.info(sipfullproxy.time.strftime("%a, %d %b %Y %H:%M:%S ", sipfullproxy.time.localtime()))
+    my_hostname = sipfullproxy.socket.gethostname()
+    sipfullproxy.logging.info(my_hostname)
+    my_ip_add = sipfullproxy.socket.gethostbyname(sipfullproxy.socket.gethostname())
+    print(my_ip_add)
+    #ipaddress = "192.168.56.1" #sipfullproxy.socket.gethostbyname(hostname)
+    #if ipaddress == "127.0.0.1":
+    #    ipaddress = sipfullproxy.sys.argv[1]
+    sipfullproxy.logging.info(my_ip_add)
+    sipfullproxy.recordroute = "Record-Route: <sip:%s:%d;lr>" % (my_ip_add,sipfullproxy.PORT)
+    sipfullproxy.topvia = "Via: SIP/2.0/UDP %s:%d" % (my_ip_add,sipfullproxy.PORT)
+    my_server = sipfullproxy.socketserver.UDPServer((sipfullproxy.HOST, sipfullproxy.PORT), sipfullproxy.UDPHandler)
     print("Proxy is active")
-    server.serve_forever()
+    my_server.serve_forever()
+    
