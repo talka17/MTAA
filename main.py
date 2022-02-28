10176e2d588cf92ad2b58b90b2fee85f43f50111
Natalia Sedova <sedova.natalia9@gmail.com>
2022-02-28 13:46:10 +0100

    edited main, adding pcap files

--- main.py
+++ main.py
@@ -1,33 +1,14 @@
 import sipfullproxy
 
-if __name__ == "__main__":    
-    #sipfullproxy.logging.info(sipfullproxy.time.strftime("%a, %d %b %Y %H:%M:%S ", sipfullproxy.time.localtime()))
-    #my_hostname = sipfullproxy.socket.gethostname()
-    #sipfullproxy.logging.info(my_hostname)
+if __name__ == "__main__":
+    #get my ip address of proxy
     my_ip_add = sipfullproxy.socket.gethostbyname(sipfullproxy.socket.gethostname())
-    #print(my_ip_add)
-    #ipaddress = "192.168.56.1" #sipfullproxy.socket.gethostbyname(hostname)
-    if my_ip_add == "127.0.0.1":
-        my_ip_add = sipfullproxy.sys.argv[1]
-    #sipfullproxy.logging.info(my_ip_add)
+    #initialization of variables from library
     sipfullproxy.recordroute = "Record-Route: <sip:%s:%d;lr>" % (my_ip_add,sipfullproxy.PORT)
     sipfullproxy.topvia = "Via: SIP/2.0/UDP %s:%d" % (my_ip_add,sipfullproxy.PORT)
+    #starting SIP proxy
     my_server = sipfullproxy.socketserver.UDPServer((sipfullproxy.HOST, sipfullproxy.PORT), sipfullproxy.UDPHandler)
-    print("SIP Proxy is active")
+    print("SIP Proxy is active. IP address:", my_ip_add)
     print("When you want to stop SIP Proxy, press Ctrl + C ")
     my_server.serve_forever()
     
-    
-#if __name__ == "__main__":    
-#    logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s',filename='proxy.log',level=logging.INFO,datefmt='%H:%M:%S')
-#    logging.info(time.strftime("%a, %d %b %Y %H:%M:%S ", time.localtime()))
-#    hostname = socket.gethostname()
-#    logging.info(hostname)
-#    ipaddress = socket.gethostbyname(hostname)
-#    if ipaddress == "127.0.0.1":
-#        ipaddress = sys.argv[1]
-#    logging.info(ipaddress)
-#    recordroute = "Record-Route: <sip:%s:%d;lr>" % (ipaddress,PORT)
-#    topvia = "Via: SIP/2.0/UDP %s:%d" % (ipaddress,PORT)
-#    server = socketserver.UDPServer((HOST, PORT), UDPHandler)
-#    server.serve_forever()
